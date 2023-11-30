c51c31d3b4bf7e30da87b0fb69b423a9b5403467
Muheb <133802123+NeelofarMuheb@users.noreply.github.com>
2023-11-30 11:47:28 -0500

    added successfully

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module NilofarMuhebEducation {
+}
---
+++ src/neloeducation/MyHighSchool.java
@@ -0,0 +1,10 @@
+package neloeducation;
+
+public class MyHighSchool {
+
+	public static void main(String[] args) {
+		System.out.println("Graduated from Lame Shaheed");
+
+	}
+
+}
